4c011099d47175ddb4fd283966b839084bd42667
shahidnad <shahidnadafco@gmail.com>
2025-09-23 13:29:45 +0530

    implemented complete and exit buttons

--- gui.py
+++ gui.py
@@ -6,22 +6,27 @@ input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
-edit_button  = sg.Button("Edit")
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button],[list_box, edit_button]],
-                   font=('Helvetica', 12)) #creates instance of window
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
+                   font=('Segoe Script', 12)) #creates instance of window
 
 
 while True: #for stopping closing window
     event, values = window.read() #uses methods of that instance
-    # print(1, event)
-    # print(2, values)
-    # print(3, values['todos'])
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
-            new_todo = values['todo'] + "\n"
+            new_todo = values['todo'] #+ "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
@@ -35,6 +40,20 @@ while True: #for stopping closing window
             window['todos'].update(values=todos)
         case "todos":
             window['todo'].update(value=values['todos'][0])
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value='')
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
 window.close()
+
+
+
+
+#exit() function stops the program completely and break used to get out of loop
